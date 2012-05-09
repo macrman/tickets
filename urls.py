7060683ba77aa70022247d11eacc91206a22ee79
Michael Ackerman <binarymac@gmail.com>
2012-05-08 17:14:18 -0700

    renamed view_ticket to ticket_detail and index to ticket_list

--- urls.py
+++ urls.py
@@ -2,19 +2,19 @@ from django.conf.urls import patterns, include, url
 from django.views.generic import ListView, DetailView
 from tickets.models import ticket
 
-
-
 urlpatterns = patterns('',
     (r'^$',
         ListView.as_view(
         model=ticket,
-        template_name="index.html",
+        template_name="ticket_list.html",
         context_object_name="ticket_list",)),
+
     (r'^(?P<pk>\d+)/$',
         DetailView.as_view(
         model = ticket,
-        template_name="view_ticket.html",
+        template_name="ticket_detail.html",
         )),
+    (r'^(P<pk>\d+)
 
 
 )
--- views.py
+++ views.py
@@ -5,14 +5,6 @@ from django.http import Http404, HttpResponse
 from django.shortcuts import render_to_response
 from django.template import Context, loader
 
-def index(request):
-    ticket_index = ticket.objects.all().order_by('date_created')[:5]
-    t = loader.get_template('index.html')
-    c = Context({
-        'ticket_index': ticket_index,
-    })
-    return HttpResponse(t.render(c))
-
 def view_ticket(request, ticket_id):
     try:
         t = ticket.objects.get(pk=ticket_id)
